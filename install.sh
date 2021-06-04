f6c82154c9046f2b247a79368e85a9d0a22713eb
AndrewDnvn <68348312+AndrewDnvn@users.noreply.github.com>
2021-06-04 09:57:59 -0400

    Updated install.sh
    Checked ubuntu compatibility.

--- install.sh
+++ install.sh
@@ -5,156 +5,160 @@
 
 red=`tput setaf 1`
 green=`tput setaf 2`
+blue=`tput setaf 4`
 reset=`tput sgr0`
 
-if[[-f /etc/redhat-release]]; then
+if [ -n "$(uname -a | grep Ubuntu)" ]; then
 
-sudo yum -y install epel-release
+	echo "${blue}Found Ubuntu. ${reset}"
+	  
+	sudo apt -y install software-properties-common
 
-wait
+	wait
 
-sudo yum -y install ansible
+	sudo add-apt-repository --yes --update ppa:ansible/ansible
 
-wait
+	wait
 
-sudo subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
+	sudo apt -y install ansible
 
-wait
+	wait
 
-sudo yum -y install python3-argcomplete
+	sudo apt -y install python-argcomplete
 
-wait
+	echo "ansible installed."
 
-echo "${red}Ansible installed.${reset}"
+	#testing the install correctly
 
-#testing the install correctly
+	ansible --version
 
-ansible --version
+	echo "${red}Installing git if not present. ${reset}"
 
-wait
+	sudo apt-get install git
 
-echo "${red}Installing docker.${reset}"
+	wait
 
-sudo yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+	echo "${red}Pulling Docker images from github ${reset}"
 
-wait 
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo yum -y install -y yum-utils
+	wait
 
-wait
+	git clone https://github.com/fabric-testbed/fabric-docker-images.git
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	wait
 
-wait
+	./set_folder.sh 
 
-sudo yum -y install docker-ce docker-ce-cli containerd.io
+	sudo sysctl -w vm.max_map_count=262144 
 
-wait
+	sudo apt-get install apache2-utils
 
-sudo systemctl start docker
+	sudo chmod +x /usr/local/bin/docker-compose
 
-wait
+	echp "${green}Checking docker-compose version ${reset}"
 
-echo "${red}Installing git if not present. ${reset}"
+	docker-compose --version
 
-sudo yum -y install git
+	fabric-docker-images/elk/setfolders.sh
 
-wait
+	echo "${red}Set folders. ${reset}"
 
-echo "${red}Pulling Docker images from github ${reset}"
+	echo "${red}Switch to correct diretory /fabric-docker-images/elk/nginx/etc. And edit the config file to set the login for the server.${reset}"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	echo "${green}Command to run: htpasswd -c .htpasswd.user f [user_namehere]${reset}"
 
-wait
+	echo "${green} Make sure to use the command sudo usermod -aG docker \$user_namehere to be able to assign the user to veiw the docker container.${reset}"
 
-git clone https://github.com/fabric-testbed/fabric-docker-images.git
+else
 
-wait
+	echo "${blue}Found CentOs. ${reset}"
 
-./set_folder.sh 
+	sudo yum -y install epel-release
 
-sudo sysctl -w vm.max_map_count=262144 
+	wait
 
-sudo yum -y install -y httpd-tools
+	sudo yum -y install ansible
 
-sudo chmod +x /usr/local/bin/docker-compose
+	wait
 
-echp "${green}Checking docker-compose version ${reset}"
+	sudo subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
 
-docker-compose --version
+	wait
 
-fabric-docker-images/elk/setfolders.sh
+	sudo yum -y install python3-argcomplete
 
-echo "${red}Set folders. ${reset}"
+	wait
 
-echo "${red}Switch to correct diretory /fabric-docker-images/elk/nginx/etc. And edit the config file to set the login for the server.${reset}"
+	echo "${red}Ansible installed.${reset}"
 
-echo "${green}Command to run: htpasswd -c .htpasswd.user f [user_namehere]${reset}"
+	#testing the install correctly
 
-echo "${green} Make sure to use the command sudo usermod -aG docker \$user_namehere to be able to assign the user to veiw the docker container.${reset}"
+	ansible --version
+
+	wait
+
+	echo "${red}Installing docker.${reset}"
+
+	sudo yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+	wait 
+
+	sudo yum -y install -y yum-utils
+
+	wait
+
+	sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+	wait
+
+	sudo yum -y install docker-ce docker-ce-cli containerd.io
+
+	wait
+
+	sudo systemctl start docker
+
+	wait
+
+	echo "${red}Installing git if not present. ${reset}"
+
+	sudo yum -y install git
+
+	wait
+
+	echo "${red}Pulling Docker images from github ${reset}"
+
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+	wait
+
+	git clone https://github.com/fabric-testbed/fabric-docker-images.git
+
+	wait
+
+	./set_folder.sh 
+
+	sudo sysctl -w vm.max_map_count=262144 
+
+	sudo yum -y install -y httpd-tools
+
+	sudo chmod +x /usr/local/bin/docker-compose
+
+	echp "${green}Checking docker-compose version ${reset}"
+
+	docker-compose --version
+
+	fabric-docker-images/elk/setfolders.sh
+
+	echo "${red}Set folders. ${reset}"
+
+	echo "${red}Switch to correct diretory /fabric-docker-images/elk/nginx/etc. And edit the config file to set the login for the server.${reset}"
+
+	echo "${green}Command to run: htpasswd -c .htpasswd.user f [user_namehere]${reset}"
+
+	echo "${green} Make sure to use the command sudo usermod -aG docker \$user_namehere to be able to assign the user to veiw the docker container.${reset}"
 
 fi
 
-if [ -f /etc/lsb-release ]; then
-  
-sudo apt -y install software-properties-common
-
-wait
-
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-
-wait
-
-sudo apt -y install ansible
-
-wait
-
-sudo apt -y install python-argcomplete
-
-echo "ansible installed."
-
-#testing the install correctly
-
-ansible --version
-
-echo "${red}Installing git if not present. ${reset}"
-
-sudo apt-get install git
-
-wait
-
-echo "${red}Pulling Docker images from github ${reset}"
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-wait
-
-git clone https://github.com/fabric-testbed/fabric-docker-images.git
-
-wait
-
-./set_folder.sh 
-
-sudo sysctl -w vm.max_map_count=262144 
-
-sudo apt-get install apache2-utils
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-echp "${green}Checking docker-compose version ${reset}"
-
-docker-compose --version
-
-fabric-docker-images/elk/setfolders.sh
-
-echo "${red}Set folders. ${reset}"
-
-echo "${red}Switch to correct diretory /fabric-docker-images/elk/nginx/etc. And edit the config file to set the login for the server.${reset}"
-
-echo "${green}Command to run: htpasswd -c .htpasswd.user f [user_namehere]${reset}"
-
-echo "${green} Make sure to use the command sudo usermod -aG docker \$user_namehere to be able to assign the user to veiw the docker container.${reset}"
-
-fi
 
 echo "${red} Installed all needed tools.${reset}"
