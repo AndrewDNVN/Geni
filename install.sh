8569f6c3c0f02a459327b0c2baf502de35bfa94c
AndrewDnvn <68348312+AndrewDnvn@users.noreply.github.com>
2021-06-10 10:51:17 -0400

    Update install.sh
    Updating working status

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# tested on Ubunutu 20.04: working 6/09/2021
+# tested on CentOs 7: working 6/09/2021
+# tested on CentOs 8: working 6/10/2021
+
 #installs ansible and the docker scripts
 #added in colored output for ease of following sctipts
 
